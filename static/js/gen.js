0313800cf12f3f01a05ed8ce81c33120d3ce884b
Chris Done <chrisdone@gmail.com>
2012-09-19 21:20:34 +0200

    Use less annoying window.alert.

--- static/js/gen.js
+++ static/js/gen.js
@@ -3,3 +3,11 @@ window.console.log = function(o){
     $('body').append($('<div class="alert alert-info"></div>').text(o));
   })
 };
+window.alert = function(o){
+  $(function(){
+    var alert = $('<div class="alert"><button type="button" class="close" data-dismiss="alert">×</button><span></span></div>');
+    $('body').append(alert);
+    alert.find('span').text(o);
+    alert.find('button').click(function(){ alert.remove() })
+  })
+};
