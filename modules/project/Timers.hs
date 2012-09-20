7c748e28b4ace4aab44069b5ccde5e27bed2d258
Chris Done <chrisdone@gmail.com>
2012-09-20 18:55:24 +0200

    Timer update.

--- modules/project/Timers.hs
+++ modules/project/Timers.hs
@@ -7,5 +7,10 @@ import Language.Fay.Prelude
 import Language.Fay.Print
 
 main = do
-  setInterval 1000 $ print "Pogo pogo pogo pogo!"
+  j <- setInterval 1000 $ print "JUMP!"
+  p <- setInterval 800 $ print "Pogo pogo pogo pogo!"
+  setTimeout 2000 $ do
+    print "Actually, forget it."
+    clearTimeout j
+    clearTimeout p
   print "Here we go!"
